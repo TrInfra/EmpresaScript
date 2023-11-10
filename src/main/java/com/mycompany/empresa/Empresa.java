128fdac1dcc23e157ba96152695b137b28ac1d85
Nyck Bastos <90692553+TrInfra@users.noreply.github.com>
2023-11-09 23:04:10 -0300

    atualizações

--- src/main/java/com/mycompany/empresa/Empresa.java
+++ src/main/java/com/mycompany/empresa/Empresa.java
@@ -1,5 +1,4 @@
 
-
 package com.mycompany.empresa;
 
 import java.text.ParseException;
@@ -8,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 /**
  *
  * @author TrInfra
@@ -22,34 +22,34 @@ public class Empresa {
         ProdutoUsado pdu = new ProdutoUsado();
         ProdutoImportado pdi = new ProdutoImportado();
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-        int j =0;
-        /*Entrada- quantidade de produtos cadastrando*/
+        int j = 0;
+        /* Entrada- quantidade de produtos cadastrando */
         System.out.println("Entre com o número de produtos:");
         Scanner sc = new Scanner(System.in);
-         int npd = sc.nextInt();
-         sc.nextLine();
-        while(j < npd){
-            System.out.println("Insira o produto #"+ (j+1));
+        int npd = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < npd; i++) {
+            System.out.println("Insira o produto #" + (j + 1));
             System.out.println("Ele é comum, usado ou importado(c/u/i)?\n");
-            String op =sc.nextLine();
-            if("c".equals(op)){
+            String op = sc.nextLine();
+            if ("c".equals(op)) {
                 System.out.println("nome:");
-                 pd.setNome(sc.nextLine());
-                 
-                System.out.println("Preço:");
-                 pd.setPreco(sc.nextInt());
-                 produtosCadastrados.add(pd);
+                pd.setNome(sc.nextLine());
 
-            }
-            else if("u".equals(op)){
-                System.out.println("nome:");
-                 pdu.setNome(sc.nextLine());              
                 System.out.println("Preço:");
-                 pdu.setPreco(sc.nextInt());
-                 sc.nextLine();
+                pd.setPreco(sc.nextInt());
+                sc.nextLine();
+                produtosCadastrados.add(pd);
+
+            } else if ("u".equals(op)) {
+                System.out.println("nome:");
+                pdu.setNome(sc.nextLine());
+                System.out.println("Preço:");
+                pdu.setPreco(sc.nextInt());
+                sc.nextLine();
                 System.out.println("Data de Fabricação: ");
                 String dataRecebida = sc.nextLine();
-                
+
                 try {
                     Date data = formato.parse(dataRecebida);
                     pdu.setLancamento(data);
@@ -58,44 +58,45 @@ public class Empresa {
                 }
                 produtosCadastrados.add(pdu);
 
-
-            }
-            else if("i".equals(op)){
+            } else if ("i".equals(op)) {
                 System.out.println("nome: ");
-                 pdi.setNome(sc.nextLine());              
+                pdi.setNome(sc.nextLine());
                 System.out.println("Preço: ");
-                 pdi.setPreco(sc.nextInt());
+                pdi.setPreco(sc.nextInt());
                 System.out.println("Taxa alfandegaria: ");
                 pdi.setTaxaAfandega(sc.nextDouble());
+                sc.nextLine();
                 produtosCadastrados.add(pdi);
 
             }
-            
+
             j++;
         }
         sc.close();
+
         for (Produtos produto : produtosCadastrados) {
-            
+
             System.out.println("--------------------");
-            
-    
-        if (produto instanceof ProdutoUsado) {
-            ProdutoUsado produtoUsado = (ProdutoUsado) produto;
-            System.out.println("\nNome: " + produto.getNome());
-            System.out.println("\nPreço: " + produto.getPreco());
-            System.out.println("\nData de Fabricação: " + formato.format(produtoUsado.getDataFabricacao()));
-        } else if (produto instanceof ProdutoImportado) {
-            ProdutoImportado produtoImportado = (ProdutoImportado) produto;
-            System.out.println("\nNome: " + produto.getNome());
-            System.out.println("\nPreço: " + produto.getPreco());
-            System.out.println("\nTaxa Alfandegária: " + produtoImportado.getTaxaAfandega());
-            System.out.println("TOTAL: preço + taxa = R$"+ pdi.precoTotal(pdi.getPreco(), pdi.getTaxaAfandega()));
-            }else if(produto instanceof Produtos){
+
+            if (produto instanceof ProdutoUsado) {
+                ProdutoUsado produtoUsado = (ProdutoUsado) produto;
+                System.out.println("\nNome: " + produto.getNome());
+                System.out.println("\nPreço: " + produto.getPreco());
+                System.out.println("\nData de Fabricação: " + formato.format(produtoUsado.getDataFabricacao()));
+            } else if (produto instanceof ProdutoImportado) {
+                ProdutoImportado produtoImportado = (ProdutoImportado) produto;
+                System.out.println("\nNome: " + produto.getNome());
+                System.out.println("\nPreço: " + produto.getPreco());
+                System.out.println("\nTaxa Alfandegária: " + produtoImportado.getTaxaAfandega());
+                System.out.println("TOTAL: preço + taxa = R$" + pdi.precoTotal(pdi.getPreco(), pdi.getTaxaAfandega()));
+            } else if (produto instanceof Produtos) {
                 System.out.println("\nNome: " + produto.getNome());
                 System.out.println("\nPreço: " + produto.getPreco());
             }
+2
             System.out.println("--------------------");
+
         }
-        
+
     }
 }
